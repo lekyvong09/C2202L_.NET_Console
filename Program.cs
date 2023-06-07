f1949e3175cfa079b05fe32de41e01884977a3f8
lekyvong09 <lekyvong09@gmail.com>
2023-06-07 20:20:52 +0700

    get user input

--- Program.cs
+++ Program.cs
@@ -7,6 +7,13 @@ class Program
     {
         //AppScreen appScreen = new AppScreen();
         AppScreen.Welcome();
+
+        string input = Utility.GetUserInput("your card number");
+
+        // "Your card number is " + input
+        Console.WriteLine($"Your card number is {input}");
+
+        Utility.PressEnterToContinue();
     }
 }
 
---
+++ UI/Utility.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace console.UI
+{
+	public class Utility
+	{
+        public static void PressEnterToContinue()
+        {
+            Console.WriteLine("\n\nPress Enter to continue...");
+            Console.ReadLine();
+        }
+
+        public static string GetUserInput(string prompt)
+        {
+            Console.WriteLine($"Enter {prompt}");
+            return Console.ReadLine();
+        }
+    }
+
+}
+
