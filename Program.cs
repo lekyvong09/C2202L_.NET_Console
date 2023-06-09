3c37b221ca3988395ebe3c79fddd283768d098a7
lekyvong09 <lekyvong09@gmail.com>
2023-06-09 20:51:59 +0700

    input card pin

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using console.Entities;
+﻿using System.Text;
+using console.Entities;
 using console.UI;
 
 namespace console;
@@ -9,35 +10,11 @@ class Program
     void InitializeData() {
         userAccounts = new List<UserAccount> {
             new UserAccount
-            {
-                Id = 1,
-                FullName = "Ray",
-                AccountNumber = 123456,
-                CardNumber = 654321,
-                CardPin = 111111,
-                AccountBalance = 10000,
-                IsLocked = false
-            },
+            { Id = 1, FullName = "Ray", AccountNumber = 123456, CardNumber = 654321, CardPin = 111111, AccountBalance = 10000, IsLocked = false },
             new UserAccount
-            {
-                Id = 2,
-                FullName = "User 2",
-                AccountNumber = 234567,
-                CardNumber = 765432,
-                CardPin = 222222,
-                AccountBalance = 20000,
-                IsLocked = false
-            },
+            { Id = 2, FullName = "User 2", AccountNumber = 234567, CardNumber = 765432, CardPin = 222222, AccountBalance = 20000, IsLocked = false },
             new UserAccount
-            {
-                Id = 3,
-                FullName = "User 3",
-                AccountNumber = 345678,
-                CardNumber = 876543,
-                CardPin = 333333,
-                AccountBalance = 30000,
-                IsLocked = false
-            }
+            { Id = 3, FullName = "User 3", AccountNumber = 345678, CardNumber = 876543, CardPin = 333333, AccountBalance = 30000, IsLocked = false }
         };
     }
 
@@ -45,6 +22,7 @@ class Program
     static void Main(string[] args)
     {
         long cardNumber = 0;
+        int cardPin = 0;
 
         AppScreen.Welcome();
 
@@ -61,8 +39,48 @@ class Program
             }
         }
 
-        // "Your card number is " + input
+        
+
+        Console.WriteLine("\n\nPress Enter your PIN to continue..");
+        StringBuilder inputPin = new StringBuilder();
+
+        while (cardPin == 0) {
+            var inputKey = Console.ReadKey(true);
+
+            if (inputKey.Key == ConsoleKey.Enter)
+            {
+                if (inputPin.Length == 6) {
+                    try {
+                        var temp = Int32.Parse(inputPin.ToString());
+                        if (temp > 0) {
+                            cardPin = temp;
+                            break;
+                        }
+                    } catch (FormatException e) {
+                        Utility.Alertify(e.Message, false);
+                        inputPin.Clear(); // reset inputPin = "";
+                        continue;
+                    }
+                } else {
+                    Utility.Alertify("\nMust be 6 digits. Please try again", false);
+                    inputPin.Clear(); // reset inputPin = "";
+                    continue;
+                }
+            }
+
+            if (inputKey.Key == ConsoleKey.Backspace && inputPin.Length > 0) {
+                inputPin.Remove(inputPin.Length - 1, 1);
+                Console.Write("\b \b");
+            } else {
+                //inputPin = inputPin + inputKey.KeyChar;
+                inputPin.Append(inputKey.KeyChar);
+                Console.Write("*");
+            }
+        }
+        
+
         Utility.Alertify($"Your card number is {cardNumber}", true);
+        Utility.Alertify($"Your card pin is {inputPin}", true);
 
         Utility.PressEnterToContinue();
     }
