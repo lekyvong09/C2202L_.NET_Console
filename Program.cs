675d1c13b263f2e415570037926eada7b8d7fc87
lekyvong09 <lekyvong09@gmail.com>
2023-06-09 21:49:33 +0700

    Check cardNumber & cardPin against database

--- Program.cs
+++ Program.cs
@@ -1,86 +1,102 @@
 ﻿using System.Text;
 using console.Entities;
+using console.Services;
 using console.UI;
 
 namespace console;
 class Program
 {
-    private List<UserAccount> userAccounts;
-
-    void InitializeData() {
-        userAccounts = new List<UserAccount> {
-            new UserAccount
-            { Id = 1, FullName = "Ray", AccountNumber = 123456, CardNumber = 654321, CardPin = 111111, AccountBalance = 10000, IsLocked = false },
-            new UserAccount
-            { Id = 2, FullName = "User 2", AccountNumber = 234567, CardNumber = 765432, CardPin = 222222, AccountBalance = 20000, IsLocked = false },
-            new UserAccount
-            { Id = 3, FullName = "User 3", AccountNumber = 345678, CardNumber = 876543, CardPin = 333333, AccountBalance = 30000, IsLocked = false }
-        };
-    }
-
-
+    
     static void Main(string[] args)
     {
         long cardNumber = 0;
         int cardPin = 0;
+        bool loginSuccess = false;
+        UserLogin userLogin = new UserLogin();
 
         AppScreen.Welcome();
+        while (loginSuccess == false) {
 
-        while (cardNumber == 0) {
-            string input = Utility.GetUserInput("your card number");
-            try
+            while (cardNumber == 0)
             {
-                cardNumber = long.Parse(input);
-            }
-            catch (FormatException e)
-            {
-                /// handle exception -- stop || display message
-                Utility.Alertify(e.Message, false);
-            }
-        }
-
-        
-
-        Console.WriteLine("\n\nPress Enter your PIN to continue..");
-        StringBuilder inputPin = new StringBuilder();
-
-        while (cardPin == 0) {
-            var inputKey = Console.ReadKey(true);
-
-            if (inputKey.Key == ConsoleKey.Enter)
-            {
-                if (inputPin.Length == 6) {
-                    try {
-                        var temp = Int32.Parse(inputPin.ToString());
-                        if (temp > 0) {
-                            cardPin = temp;
-                            break;
-                        }
-                    } catch (FormatException e) {
-                        Utility.Alertify(e.Message, false);
-                        inputPin.Clear(); // reset inputPin = "";
-                        continue;
-                    }
-                } else {
-                    Utility.Alertify("\nMust be 6 digits. Please try again", false);
-                    inputPin.Clear(); // reset inputPin = "";
-                    continue;
+                string input = Utility.GetUserInput("your card number");
+                try
+                {
+                    cardNumber = long.Parse(input);
+                }
+                catch (FormatException e)
+                {
+                    /// handle exception -- stop || display message
+                    Utility.Alertify(e.Message, false);
                 }
             }
 
-            if (inputKey.Key == ConsoleKey.Backspace && inputPin.Length > 0) {
-                inputPin.Remove(inputPin.Length - 1, 1);
-                Console.Write("\b \b");
-            } else {
-                //inputPin = inputPin + inputKey.KeyChar;
-                inputPin.Append(inputKey.KeyChar);
-                Console.Write("*");
+            Console.WriteLine("\n\nPress Enter your PIN to continue..");
+            StringBuilder inputPin = new StringBuilder();
+
+            while (cardPin == 0)
+            {
+                var inputKey = Console.ReadKey(true);
+
+                if (inputKey.Key == ConsoleKey.Enter)
+                {
+                    if (inputPin.Length == 6)
+                    {
+                        try
+                        {
+                            var temp = Int32.Parse(inputPin.ToString());
+                            if (temp > 0)
+                            {
+                                cardPin = temp;
+                                break;
+                            }
+                            else
+                            {
+                                Utility.Alertify("\nMust be 6 digits. Please try again", false);
+                                inputPin.Clear(); // reset inputPin = "";
+                                continue;
+                            }
+                        }
+                        catch (FormatException e)
+                        {
+                            Utility.Alertify(e.Message, false);
+                            inputPin.Clear(); // reset inputPin = "";
+                            continue;
+                        }
+                    }
+                    else
+                    {
+                        Utility.Alertify("\nMust be 6 digits. Please try again", false);
+                        inputPin.Clear(); // reset inputPin = "";
+                        continue;
+                    }
+                }
+
+                if (inputKey.Key == ConsoleKey.Backspace && inputPin.Length > 0)
+                {
+                    inputPin.Remove(inputPin.Length - 1, 1);
+                    Console.Write("\b \b");
+                }
+                else
+                {
+                    //inputPin = inputPin + inputKey.KeyChar;
+                    inputPin.Append(inputKey.KeyChar);
+                    Console.Write("*");
+                }
+            }
+
+
+            Utility.Alertify($"Your card number is {cardNumber}", true);
+            Utility.Alertify($"Your card pin is {inputPin}", true);
+
+            loginSuccess = userLogin.CheckUserCardNumAndPassword(cardNumber, cardPin);
+
+            if (!loginSuccess) {
+                cardNumber = 0;
+                cardPin = 0;
             }
         }
-        
 
-        Utility.Alertify($"Your card number is {cardNumber}", true);
-        Utility.Alertify($"Your card pin is {inputPin}", true);
 
         Utility.PressEnterToContinue();
     }
---
+++ Services/UserLogin.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using console.Entities;
+using console.UI;
+
+namespace console.Services
+{
+	public class UserLogin
+	{
+
+        private List<UserAccount> userAccounts;
+
+        public UserLogin() {
+            InitializeData();
+        }
+
+        void InitializeData() {
+            userAccounts = new List<UserAccount> {
+                new UserAccount
+                { Id = 1, FullName = "Ray", AccountNumber = 123456, CardNumber = 654321, CardPin = 111111, AccountBalance = 10000, IsLocked = false },
+                new UserAccount
+                { Id = 2, FullName = "User 2", AccountNumber = 234567, CardNumber = 765432, CardPin = 222222, AccountBalance = 20000, IsLocked = false },
+                new UserAccount
+                { Id = 3, FullName = "User 3", AccountNumber = 345678, CardNumber = 876543, CardPin = 333333, AccountBalance = 30000, IsLocked = false }
+            };
+        }
+
+        public bool CheckUserCardNumAndPassword(long cardNumber, int cardPin) {
+            bool isLoginSuccess = false;
+
+            /// verifying....
+            Console.WriteLine("\nVerifying...");
+            for (int i = 0; i < 10; i++) {
+                Console.Write(".");
+                Thread.Sleep(200);
+            }
+            Console.Clear();
+
+            //Console.WriteLine($"Check number of account {userAccounts.Count()}");
+            
+            userAccounts.Where(element => element.CardNumber == cardNumber).FirstOrDefault();
+            UserAccount account = userAccounts.Find(element => element.CardNumber == cardNumber);
+
+            if (account != null) {
+                account.TotalLogin++;
+
+                if (account.IsLocked) {
+                    Utility.Alertify("\nYour account is locked. Please contact our nearest office.\n", false);
+                    Environment.Exit(1); /// force to exit Application
+                }
+
+                if (account.CardPin == cardPin) {
+                    isLoginSuccess = true;
+                    account.TotalLogin = 0;
+                } else {
+                    if (account.TotalLogin > 3) {
+                        account.IsLocked = true;
+                        Utility.Alertify("\nYour account is locked. Please contact our nearest office.\n", false);
+                        Environment.Exit(1); /// force to exit Application
+                    } else {
+                        Utility.Alertify("\nInvalid card number or PIN! \n", false);
+                    }
+                }
+            } else {
+                Utility.Alertify("\nInvalid card number or PIN! \n", false);
+            }
+
+            return isLoginSuccess;
+        }
+
+
+        
+	}
+}
+
