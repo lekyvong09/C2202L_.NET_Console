9b7d870f4c413e6e2d279913a3d8a62712cc8f0b
lekyvong09 <lekyvong09@gmail.com>
2023-06-12 20:54:40 +0700

    refactoring cont..

--- Program.cs
+++ Program.cs
@@ -17,8 +17,8 @@ class Program
         AppScreen.Welcome();
         while (loginSuccess == false)
         {
-            cardNumber = InputCardNumberScreen();
-            cardPin = InputCardPinScreen();
+            cardNumber = AppScreen.InputCardNumberScreen();
+            cardPin = AppScreen.InputCardPinScreen();
 
             Utility.Alertify($"Your card number is {cardNumber}", true);
             Utility.Alertify($"Your card pin is {cardPin}", true);
@@ -27,93 +27,11 @@ class Program
 
         }
 
-
         Console.WriteLine($"Welcome back, {userLogin.CurrentActiveUser.FullName}");
         Utility.PressEnterToContinue();
 
-        /// welcome user message....
-        /// deposit -> balance + amount
     }
 
-    private static long InputCardNumberScreen()
-    {
-        long cardNumber = 0;
-        while (cardNumber == 0)
-        {
-            string input = Utility.GetUserInput("your card number");
-            try
-            {
-                cardNumber = long.Parse(input);
-            }
-            catch (FormatException e)
-            {
-                /// handle exception -- stop || display message
-                Utility.Alertify(e.Message, false);
-            }
-        }
-
-        return cardNumber;
-    }
-
-
-    private static int InputCardPinScreen()
-    {
-        int cardPin = 0;
-        Console.WriteLine("\n\nPress Enter your PIN to continue..");
-        StringBuilder inputPin = new StringBuilder();
-
-        while (cardPin == 0)
-        {
-            var inputKey = Console.ReadKey(true);
-
-            if (inputKey.Key == ConsoleKey.Enter)
-            {
-                if (inputPin.Length == 6)
-                {
-                    try
-                    {
-                        var temp = Int32.Parse(inputPin.ToString());
-                        if (temp > 0)
-                        {
-                            cardPin = temp;
-                            break;
-                        }
-                        else
-                        {
-                            Utility.Alertify("\nMust be 6 digits. Please try again", false);
-                            inputPin.Clear(); // reset inputPin = "";
-                            continue;
-                        }
-                    }
-                    catch (FormatException e)
-                    {
-                        Utility.Alertify(e.Message, false);
-                        inputPin.Clear(); // reset inputPin = "";
-                        continue;
-                    }
-                }
-                else
-                {
-                    Utility.Alertify("\nMust be 6 digits. Please try again", false);
-                    inputPin.Clear(); // reset inputPin = "";
-                    continue;
-                }
-            }
-
-            if (inputKey.Key == ConsoleKey.Backspace && inputPin.Length > 0)
-            {
-                inputPin.Remove(inputPin.Length - 1, 1);
-                Console.Write("\b \b");
-            }
-            else
-            {
-                //inputPin = inputPin + inputKey.KeyChar;
-                inputPin.Append(inputKey.KeyChar);
-                Console.Write("*");
-            }
-        }
-
-        return cardPin;
-    }
+    
 }
 
--- UI/AppScreen.cs
+++ UI/AppScreen.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text;
+
 namespace console.UI
 {
     public static class AppScreen
@@ -12,6 +14,87 @@ namespace console.UI
             Console.WriteLine("\n\n------Welcome to my console app!-----");
             Console.WriteLine("\nPlease insert your card");
             Utility.PressEnterToContinue();
+        }
+
+        public static long InputCardNumberScreen()
+        {
+            long cardNumber = 0;
+            while (cardNumber == 0)
+            {
+                string input = Utility.GetUserInput("your card number");
+                try
+                {
+                    cardNumber = long.Parse(input);
+                }
+                catch (FormatException e)
+                {
+                    /// handle exception -- stop || display message
+                    Utility.Alertify(e.Message, false);
+                }
+            }
+
+            return cardNumber;
+        }
+
+
+        public static int InputCardPinScreen()
+        {
+            int cardPin = 0;
+            Console.WriteLine("\n\nPress Enter your PIN to continue..");
+            StringBuilder inputPin = new StringBuilder();
+
+            while (cardPin == 0)
+            {
+                var inputKey = Console.ReadKey(true);
+
+                if (inputKey.Key == ConsoleKey.Enter)
+                {
+                    if (inputPin.Length == 6)
+                    {
+                        try
+                        {
+                            var temp = Int32.Parse(inputPin.ToString());
+                            if (temp > 0)
+                            {
+                                cardPin = temp;
+                                break;
+                            }
+                            else
+                            {
+                                Utility.Alertify("\nMust be 6 digits. Please try again", false);
+                                inputPin.Clear(); // reset inputPin = "";
+                                continue;
+                            }
+                        }
+                        catch (FormatException e)
+                        {
+                            Utility.Alertify(e.Message, false);
+                            inputPin.Clear(); // reset inputPin = "";
+                            continue;
+                        }
+                    }
+                    else
+                    {
+                        Utility.Alertify("\nMust be 6 digits. Please try again", false);
+                        inputPin.Clear(); // reset inputPin = "";
+                        continue;
+                    }
+                }
+
+                if (inputKey.Key == ConsoleKey.Backspace && inputPin.Length > 0)
+                {
+                    inputPin.Remove(inputPin.Length - 1, 1);
+                    Console.Write("\b \b");
+                }
+                else
+                {
+                    //inputPin = inputPin + inputKey.KeyChar;
+                    inputPin.Append(inputKey.KeyChar);
+                    Console.Write("*");
+                }
+            }
+
+            return cardPin;
         }
 
     }
